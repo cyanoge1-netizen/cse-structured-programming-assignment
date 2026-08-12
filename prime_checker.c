d529243bdae03fb81800b58ff44dca676a0b2949
cyanoge1-netizen <2.41326137e+08+cyanoge1-netizen@users.noreply.github.com>
2026-08-12 10:05:20 +0000

    Add prime number checker C program

---
+++ prime_checker.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, isPrime = 1;
+
+    printf("Enter a positive integer: ");
+    if (scanf("%d", &n) != 1) {
+        printf("Invalid input.\n");
+        return 1;
+    }
+
+    if (n <= 1) {
+        isPrime = 0; // 0 and 1 are not prime numbers
+    } else {
+        for (i = 2; i <= n / 2; ++i) {
+            if (n % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+    if (isPrime)
+        printf("%d is a prime number.\n", n);
+    else
+        printf("%d is not a prime number.\n", n);
+
+    return 0;
+}
